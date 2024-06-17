ace37151ad1ca8ef80084054eaf99fb693d66050
polukasburfeterders3468 <139312002+polukasburfeterders3468@users.noreply.github.com>
2024-06-17 20:48:13 +0300

    config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-$bot_token = ""; /* bot token */
-$chat_id = ""; /* chatid */
+$bot_token = "6897154681:AAGVt9-oDC0nrR9p2ryZu9AUMeyU3EX25W8"; /* bot token */
+$chat_id = "6393672542"; /* chatid */
 
 ?>
